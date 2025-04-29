ec950807d73f76e46a7c48bc4f49e25397c029f4
Shekhar Mali <shekharmali2001@gmail.com>
2025-04-29 17:14:00 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -9,9 +9,9 @@ const HeroSection = () => {
         <div className='HeroSection-container'>
 
             <div className="inside-HeroSection">
-                <h1><FaDotCircle style={{ color: 'red' }} />I AM web developer</h1>
-                <span>Creative Design and Web SolutionsDesign</span>
-                <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et.</p>
+                <h1><FaDotCircle style={{ color: 'red' }} />Hello I’m Shekhar Mali </h1>
+                <span>Frontend & MERN-Stack Web Developer | Crafting Dynamic, Responsive UIs</span>
+                <p>Hello, I’m Shekhar Mali, a passionate Frontend & MERN-Stack Web Developer and recent graduate. I specialize in HTML, CSS, and JavaScript for crafting engaging UIs, complemented by React for building dynamic interfaces. On the backend, I work with Node.js, Express, MongoDB, and JWT for secure, scalable applications. Skilled in modern development workflows, I focus on writing clean, maintainable code with attention to performance, accessibility, and responsive design. Throughout my studies and hands-on exercises, I’ve cultivated a user-first approach and strong problem-solving abilities. Eager to learn and contribute, I’m excited to apply my skills to build innovative digital experiences and solutions.</p>
                 <button>Dowload My Cv</button>
             </div>
 
